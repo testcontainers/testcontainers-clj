a72aea8c305cc54a64d64697312f829de80a92b8
Burin Choomnuan <agilecreativity@gmail.com>
2020-07-12 13:21:01 -0400

    Revert "Add lein-cljfmt plugin"
    This reverts commit bff2e04625203e95b76b734e73ad58064f83944e.

--- project.clj
+++ project.clj
@@ -3,10 +3,11 @@
   :url "https://github.com/javahippie/clj-test-containers"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.testcontainers/testcontainers "1.14.3"]]
-  :plugins [[metosin/bat-test "0.4.4"]
-            [lein-cljfmt "0.6.7"]]
-  :bat-test {:report [:pretty {:type :junit
+  :plugins [[metosin/bat-test "0.4.4"]]
+  :bat-test {:report [:pretty {:type :junit 
                                :output-to "target/junit.xml"}]}
   :target-path "target/%s")
+
