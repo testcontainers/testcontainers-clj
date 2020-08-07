8ea2392ba59717fd94ab8779aa446d78003672fe
Rob Hanlon <robhanlon@gmail.com>
2020-08-07 10:58:40 -0700

    Switch from bat-test to kaocha

--- project.clj
+++ project.clj
@@ -1,14 +1,20 @@
 (defproject clj-test-containers "0.2.0-SNAPSHOT"
   :description "A lightweight, unofficial wrapper around the Testcontainers Java library"
+
   :url "https://github.com/javahippie/clj-test-containers"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.testcontainers/testcontainers "1.14.3"]]
-  :plugins [[metosin/bat-test "0.4.4"]]
-  :bat-test {:report [:pretty {:type :junit
-                               :output-to "target/junit.xml"}]}
-  :profiles {:dev {:dependencies [[org.testcontainers/postgresql "1.14.3"]]}}
+
+  :aliases {"test" ["run" "-m" "kaocha.runner"]}
+
+  :profiles {:dev {:dependencies [[org.testcontainers/postgresql "1.14.3"]
+                                  [lambdaisland/kaocha-cloverage "1.0-45"]
+                                  [lambdaisland/kaocha "1.0.641"]
+                                  [lambdaisland/kaocha-junit-xml "0.0.76"]]}}
+
   :target-path "target/%s")
 
