2b8d4a911e7b46c3570503495fcef05b19e78641
Tim Zöller <tim.zoeller@ilume.de>
2021-03-04 15:42:08 +0100

    #49 - Bumped to Testcontainers 1.15.2

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.testcontainers/testcontainers "1.15.1"]]
+                 [org.testcontainers/testcontainers "1.15.2"]]
 
   :aliases {"test" ["run" "-m" "kaocha.runner"]
             "cljstyle" ["run" "-m" "cljstyle.main"]}
@@ -18,11 +18,10 @@
                                   [lambdaisland/kaocha "1.0.641"]
                                   [lambdaisland/kaocha-cloverage "1.0-45"]
                                   [lambdaisland/kaocha-junit-xml "0.0.76"]
-                                  [lambdaisland/kaocha-junit-xml "0.0.76"]
                                   [mvxcvi/cljstyle "0.14.0" :exclusions [org.clojure/clojure]]
                                   [org.clojure/test.check "1.1.0"]
                                   [org.clojure/tools.namespace "1.0.0"]
-                                  [org.testcontainers/postgresql "1.15.1"]
+                                  [org.testcontainers/postgresql "1.15.2"]
                                   [com.fzakaria/slf4j-timbre "0.3.20"]]
                    :source-paths ["dev-src"]}}
 
