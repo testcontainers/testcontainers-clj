1297054b773ebdb495f7d5c83795fa942080b5d1
Tim Zöller <tim.zoeller@lambdaschmiede.com>
2024-04-13 09:32:57 +0200

    Updated dependency versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-test-containers "unspecified"
+(defproject testcontainers-clj "unspecified"
   :description "A lightweight, official wrapper around the Testcontainers Java library"
 
   :url "https://github.com/testcontainers/testcontainers-clj"
@@ -7,7 +7,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.testcontainers/testcontainers "1.19.0"]]
+                 [org.testcontainers/testcontainers "1.19.7"]]
 
   :aliases {"test" ["run" "-m" "kaocha.runner"]
             "cljstyle" ["run" "-m" "cljstyle.main"]}
@@ -15,15 +15,15 @@
   :plugins [[jainsahab/lein-githooks "1.0.0"]]
 
   :profiles {:dev {:dependencies [[expound "0.9.0"]
-                                  [lambdaisland/kaocha "1.85.1342"]
+                                  [lambdaisland/kaocha "1.88.1376"]
                                   [lambdaisland/kaocha-cloverage "1.1.89"]
                                   [lambdaisland/kaocha-junit-xml "1.17.101"]
-                                  [mvxcvi/cljstyle "0.15.0" :exclusions [org.clojure/clojure]]
+                                  [mvxcvi/cljstyle "0.16.630" :exclusions [org.clojure/clojure]]
                                   [org.clojure/test.check "1.1.1"]
                                   [orchestra "2021.01.01-1"]
-                                  [org.clojure/tools.namespace "1.3.0"]
-                                  [org.testcontainers/postgresql "1.19.0"]
-                                  [com.fzakaria/slf4j-timbre "0.3.21"]
+                                  [org.clojure/tools.namespace "1.5.0"]
+                                  [org.testcontainers/postgresql "1.19.7"]
+                                  [com.fzakaria/slf4j-timbre "0.4.1"]
                                   [nrepl "1.0.0"]]
                    :source-paths ["dev-src"]}
              :release {:deploy-repositories [["maven" {:url           "https://oss.sonatype.org/service/local/staging/deploy/maven2"
